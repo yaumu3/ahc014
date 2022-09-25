c6d0d92b199718bc1d53f5c06ecbd79905e72ad4
yaumu3 <yumu@me.com>
2022-09-25 17:09:14 +0900

    feat: Do/Undo rect

--- src/main.rs
+++ src/main.rs
@@ -120,26 +120,32 @@ impl Direction {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 struct DirectedPoint {
-    is_point_occupied: bool,
-    are_directions_occupied: u8,
+    is_point_used: bool,
+    are_directions_used: u8,
 }
 impl DirectedPoint {
     fn new() -> Self {
         Self {
-            is_point_occupied: false,
-            are_directions_occupied: 0,
+            is_point_used: false,
+            are_directions_used: 0,
         }
     }
-    fn occupy_point(&mut self) {
-        self.is_point_occupied |= true;
+    fn use_point(&mut self) {
+        self.is_point_used |= true;
     }
-    fn occupy_direction_at(&mut self, dir: Direction) {
-        self.are_directions_occupied |= 1 << dir.as_idx();
+    fn clear_point(&mut self) {
+        self.is_point_used &= false;
     }
-    fn is_direction_occupied_at(&self, dir: Direction) -> bool {
-        self.are_directions_occupied >> dir.as_idx() & 1 == 1
+    fn use_direction_at(&mut self, dir: Direction) {
+        self.are_directions_used |= 1 << dir.as_idx();
+    }
+    fn clear_direction_at(&mut self, dir: Direction) {
+        self.are_directions_used &= !(1 << dir.as_idx());
+    }
+    fn is_direction_used_at(&self, dir: Direction) -> bool {
+        self.are_directions_used >> dir.as_idx() & 1 == 1
     }
 }
 
@@ -165,21 +171,26 @@ impl std::fmt::Display for Rect {
 struct State {
     n: usize,
     points: Vec<Vec<DirectedPoint>>,
+    rects_history: Vec<Rect>,
 }
 impl State {
     fn new(input: &Input) -> Self {
         let n = input.n;
         let mut points = vec![vec![DirectedPoint::new(); n]; n];
         for p in input.ps.iter() {
-            points[p.x as usize][p.y as usize].occupy_point();
+            points[p.x as usize][p.y as usize].use_point();
         }
-        Self { n, points }
+        Self {
+            n,
+            points,
+            rects_history: vec![],
+        }
     }
     fn get_legal_rects(&self) -> Vec<Rect> {
         let mut result = vec![];
         for (i, row) in self.points.iter().enumerate() {
             for (j, p) in row.iter().enumerate() {
-                if p.is_point_occupied {
+                if p.is_point_used {
                     continue;
                 }
                 for d in Direction::to_vec() {
@@ -201,9 +212,9 @@ impl State {
                         let p = &self.points[ii][jj];
 
                         // In case the point is not occupied.
-                        if !(p.is_point_occupied || ii == i && jj == j) {
-                            if p.is_direction_occupied_at(cur_d)
-                                || p.is_direction_occupied_at(cur_d.flipped())
+                        if !(p.is_point_used || ii == i && jj == j) {
+                            if p.is_direction_used_at(cur_d)
+                                || p.is_direction_used_at(cur_d.flipped())
                             {
                                 break;
                             } else {
@@ -214,8 +225,7 @@ impl State {
                         // In case the point is occupied.
                         cur_d = cur_d.rotated();
                         cur_d_p2d = cur_d_p2d.rotated();
-                        if p.is_direction_occupied_at(cur_d)
-                            || p.is_direction_occupied_at(cur_d.rotated())
+                        if p.is_direction_used_at(cur_d) || p.is_direction_used_at(cur_d.rotated())
                         {
                             break;
                         }
@@ -239,8 +249,12 @@ impl State {
         }
         result
     }
-    fn applied_rect(&self, rect: &Rect) -> Result<Self, String> {
-        let mut result = self.clone();
+    fn apply_rect(
+        &mut self,
+        rect: &Rect,
+        f: fn(&mut DirectedPoint, Direction),
+        g: fn(&mut DirectedPoint),
+    ) {
         let init_i = rect.p1.x as usize;
         let init_j = rect.p1.y as usize;
 
@@ -249,34 +263,46 @@ impl State {
         let mut cur_d_p2d = cur_d.as_p2d();
         loop {
             cur_pos = cur_pos + cur_d_p2d;
-            if cur_pos.x < 0
-                || cur_pos.x >= self.n as i32
-                || cur_pos.y < 0
-                || cur_pos.y >= self.n as i32
-            {
-                return Err("Out of bounds".to_owned());
-            }
             let ii = cur_pos.x as usize;
             let jj = cur_pos.y as usize;
-            let p = &mut result.points[ii][jj];
+            let p = &mut self.points[ii][jj];
 
-            if !(p.is_point_occupied || ii == init_i && jj == init_j) {
-                p.occupy_direction_at(cur_d);
-                p.occupy_direction_at(cur_d.flipped());
+            if !(p.is_point_used || ii == init_i && jj == init_j) {
+                f(p, cur_d);
+                f(p, cur_d.flipped());
                 continue;
             }
 
             cur_d = cur_d.rotated();
             cur_d_p2d = cur_d_p2d.rotated();
-            p.occupy_direction_at(cur_d);
-            p.occupy_direction_at(cur_d.rotated());
+            f(p, cur_d);
+            f(p, cur_d.rotated());
 
             if cur_pos.x as usize == init_i && cur_pos.y as usize == init_j {
-                p.occupy_point();
+                g(p);
                 break;
             }
         }
-        Ok(result)
+    }
+    fn set_rect(&mut self, rect: &Rect) {
+        self.apply_rect(
+            rect,
+            |p: &mut DirectedPoint, d: Direction| p.use_direction_at(d),
+            |p: &mut DirectedPoint| p.use_point(),
+        );
+        self.rects_history.push(*rect);
+    }
+    fn undo_rect(&mut self) -> Result<Rect, String> {
+        if self.rects_history.is_empty() {
+            return Err("No rects used".to_owned());
+        }
+        let last_rect = self.rects_history.pop().unwrap();
+        self.apply_rect(
+            &last_rect,
+            |p: &mut DirectedPoint, d: Direction| p.clear_direction_at(d),
+            |p: &mut DirectedPoint| p.clear_point(),
+        );
+        Ok(last_rect)
     }
 }
 
@@ -296,21 +322,27 @@ fn parse_input() -> Input {
 
 fn main() {
     let input = parse_input();
+    let centroid = P::new((input.n as i32 - 1) / 2, (input.n as i32 - 1) / 2);
     let mut state = State::new(&input);
 
-    let mut result = vec![];
     loop {
         let mut rects = state.get_legal_rects();
+        rects.sort_unstable_by_key(|r| {
+            let dist = r.p1 - centroid;
+            dist.x.abs() + dist.y.abs()
+        });
         if let Some(r) = rects.pop() {
-            result.push(r);
-            state = state.applied_rect(&r).unwrap();
+            state.set_rect(&r);
         } else {
             break;
         }
     }
 
-    println!("{}", result.len());
-    for r in result {
+    println!("{}", state.rects_history.len());
+    for r in &state.rects_history {
         println!("{}", r);
     }
+
+    // while state.undo_rect().is_ok() {}
+    // dbg!(state.rects_history);
 }
